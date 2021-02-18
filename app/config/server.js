eda1cced493ba4014de7b8f7b8135b0829d056a0
pooja reddy <poojareddygopu@gmail.com>
2021-02-18 17:34:33 -0600

    docker-setup

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1a45f906f656275ae70807c032eee81'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c044e94-d008-405e-9184-e39cc4a63a78'
+};
